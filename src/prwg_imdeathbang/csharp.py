6ccb6a74269c94c53729d7c25f5b8b09e4ca0855
imdeathbang <imdeathbang@gmail.com>
2026-04-27 16:44:26 -0500

    csharp support phase 2

--- src/prwg_imdeathbang/csharp.py
+++ src/prwg_imdeathbang/csharp.py
@@ -1,44 +1,79 @@
 from prwg_imdeathbang.data import *
 
-def get_params_data(params: list[Param]) -> str:
+def get_params_data(params: list[Param], spaces: int = 4, declare: bool = True) -> str:
     data: list[str] = []
+    if not declare:
+        for param in params:
+            data.append(f"{param.name}")
+        return ", ".join(data)
+
     for param in params:
-        data.append(f"    {param.type} {param.name}")
+        data.append(f"{" " * spaces}{param.type} {param.name}")
     return ",\n".join(data)
 
-def get_library_import(library_name, params: list[Param]) -> str:
+def get_library_import(library_name, params: list[Param] = [], return_type: str = "") -> str:
     data: list[str] = []
-    data.append(f'{library_name}')
+    data.append(f'"{library_name}"')
     for param in params:
         if param.type == "string":
             data.append("StringMarshalling = StringMarshalling.Utf16")
-    return f'[LibraryImport({", ".join(data)})]'
+    if return_type == "string":
+        data.append("StringMarshalling = StringMarshalling.Utf16")
+    return_marshal = ""
+    if return_type == "bool":
+        return_marshal = "\n[return: MarshalAs(UnmanagedType.I1)]"
+    call_convention = "[UnmanagedCallConv(CallConvs = new Type[] { typeof(CallConvCdecl) })]\n    "
+    return f'{call_convention}[LibraryImport({", ".join(data)})]{return_marshal}'
+
 
 def get_interop_commands_data(library_name: str, handle: Handle) -> str:
     data: list[str] = []
     for command in handle.commands:
-        data.append(f'    {get_library_import(library_name, command.params)}')
+        data.append(f'    {get_library_import(library_name, command.params, command.type)}')
         data.append(f"    private static partial {command.type} {command.name}(")
-        data.append(f"    {handle.type} {handle.name}")
+        data.append(f"    IntPtr {handle.name}")
         data.append(get_params_data(command.params))
         data.append("    );\n")
     for property in handle.properties:
-        data.append(f'    {get_library_import(library_name, property.params)}')
+        data.append(f'    {get_library_import(library_name, return_type=property.type)}')
         data.append(f"    private static partial {property.type} {property.get_name}(")
-        data.append(f"        {handle.type} {handle.name}")
+        data.append(f"        IntPtr {handle.name}")
         data.append("    );\n")
-        data.append(f'    {get_library_import(library_name, property.params)}')
-        data.append(f"    private static partial void {property.get_name}(")
-        data.append(f"        {handle.type} {handle.name}")
+        return_param = Param(property.type, property.name)
+        data.append(f'    {get_library_import(library_name, [return_param])}')
+        data.append(f"    private static partial void {property.set_name}(")
+        data.append(f"        IntPtr {handle.name},")
         data.append(f"        {property.type} {property.name}")
         data.append("    );\n")
     return "\n".join(data)
 
+def get_constructor_data(handle: Handle) -> str:
+    data: list[str] = []
+    constructor = handle.constructor
+    data.append(f"    public {handle.type}({get_params_data(constructor.params, spaces=0)}) {{")
+    if constructor.options == ConstructorOptions.RETURN_INSTANCE:
+        params_data = get_params_data(constructor.params, spaces=0, declare=False)
+        data.append(f"    handle = {constructor.command}({params_data});")
+    elif constructor.options == ConstructorOptions.RETURN_RESULT_OUT_INSTANCE:
+        pass
+    else:
+        pass
+    data.append("    }")
+    return "\n".join(data)
+
+def get_members_data(handle: Handle) -> str:
+    data: list[str] = []
+    data.append(f"    IntPtr handle;")
+    return "\n".join(data)
+
 def get_handle_data(library_name: str, handle: Handle) -> str:
     data: list[str] = []
-    data.append("using System.Runtime.InteropServices;\n")
+    data.append("using System.Runtime.InteropServices;")
+    data.append("using System.Runtime.CompilerServices;\n")
     data.append(f"public partial class {handle.type} {{\n")
     data.append(get_interop_commands_data(library_name, handle))
+    data.append(get_members_data(handle))
+    data.append(get_constructor_data(handle))
     data.append("}")
     return "\n".join(data)
 
