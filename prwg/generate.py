7142f289fd60c5d14589d320bc9c4a5c5a2b25b0
imdeathbang <imdeathbang@gmail.com>
2026-05-19 16:56:46 -0500

    refactor part 2

--- prwg/generate.py
+++ prwg/generate.py
@@ -1,9 +1,29 @@
-from prwg.data import *
+from prwg.languages.language import Language
 import prwg.parser as parser
+from importlib import util
+from pathlib import Path
+from prwg.data import *
+
 import argparse
 
 def build(window_data: InitializeInfo):
     pass
+
+def load_language(path: Path) -> Language:
+    spec = util.spec_from_file_location(path.stem, path)
+    module = util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    for name in dir(module):
+        obj = getattr(module, name)
+        if isinstance(obj, type) and issubclass(obj, Language) and obj is not Language:
+            return obj()
+    return None
+
+def load_languages(languages_folder: Path) -> list[Language]:
+    languages: list[Language] = []
+    for file_path in languages_folder.glob("*.py"):
+        languages.append(load_language(file_path))
+    return languages
 
 def main():
     parser = argparse.ArgumentParser()
